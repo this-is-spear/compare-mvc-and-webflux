29a3ee505d17704f2418d1be2030a2e06b886814
this-is-spear <rjsckdd12@gmail.com>
2023-03-28 02:12:08 +0900

    성능 테스트 도구 구현

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # compare-mvc-and-webflux
 코드 실행을 위해 필요한 라이브러리를 결정하기 위한 비교 분석 자료입니다.
 
+## 필요한 도구
+
+- JDK 17
+- k6
+
 ## 설명
 
-MVC + Async + Polling 방식의 구조와 Webflux의 구조를 비교하기 위해 비슷한 환경을 구성했습니다.
+- MVC + Async + Polling 방식의 구조와 Webflux의 구조를 비교하기 위해 비슷한 환경을 구성했습니다.
+- k6를 활용해 성능을 테스트할 수 있습니다. 서버 영역 모니터링은 구현하지 않았습니다.
 
 
 ## MVC + Async + Polling 방식 빌드
@@ -12,8 +18,36 @@ MVC + Async + Polling 방식의 구조와 Webflux의 구조를 비교하기 위�
 cd delivery-request-mvc && ./gradlew build
 ```
 
+## MVC + Async + Polling 방식 테스트
+
+### 로드 테스트
+
+```shell
+k6 run mvc-k6-test/load.js
+```
+
+### 스트레스 테스트
+
+```shell
+k6 run mvc-k6-test/stress.js
+```
+
 ## Webflux 방식 빌드
 
 ```shell
 cd delivery-request-mvc && ./gradlew build
+```
+
+## Webflux 방식 테스트
+
+### 로드 테스트
+
+```shell
+k6 run webflux-k6-test/load.js
+```
+
+### 스트레스 테스트
+
+```shell
+k6 run webflux-k6-test/stress.js
 ```
---
+++ webflux-k6-test/stress.js
@@ -0,0 +1,45 @@
+import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '1m', target: 4 },
+    { duration: '1m', target: 6 },
+    { duration: '5m', target: 10 },
+    { duration: '5m', target: 20 },
+    { duration: '5m', target: 40 },
+    { duration: '5m', target: 80 },
+    { duration: '1m', target: 120 },
+  ],
+  thresholds: {
+    http_req_duration: ['p(99)<6500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'http://localhost:8080';
+const params = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+const data = { code: 'class Solution {\n' +
+      '\tpublic int[] solution(int brown, int yellow) {\n' +
+      '\t\tfor (int x = 1; x < (brown + 4) / 2; x++) {\n' +
+      '\t\t\tint y = (brown + 4) / 2 - x;\n' +
+      '\t\t\tif ((x - 2) * (y - 2) == yellow) {\n' +
+      '\t\t\t\tif (x > y) {\n' +
+      '\t\t\t\t\treturn new int[] {x, y};\n' +
+      '\t\t\t\t}\n' +
+      '\t\t\t\treturn new int[] {y, x};\n' +
+      '\t\t\t}\n' +
+      '\t\t}\n' +
+      '\t\tint[] answer = {-1, -1};\n' +
+      '\t\treturn answer;\n' +
+      '\t}\n' +
+      '}' };
+
+export default function () {
+  // Request Execute Code
+  let executeCode = http.post(`${BASE_URL}/code`, JSON.stringify(data), params);
+  check(executeCode, {"Execute Code API status check": (resp) => resp.status === 200});
+}
